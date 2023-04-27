516222492ea4786c1efe991d87e47486204f16b7
Edmund Bennett <bennettedmund@gmail.com>
2023-04-27 19:46:51 +0100

    Added protractor and fixed title

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('RockPaperScienceUi app is running!');
+    expect(await page.getTitleText()).toEqual('Rock Paper Science app is running!');
   });
 
   afterEach(async () => {
